91d652990fb60678a9112fdde65c1fcac5c62467
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-01-16 11:22:28 -0500

    Revert "implement registration"
    This reverts commit fcf041208d29a9209c2e5d93c0933dc9bce04786.

--- final_project/index.js
+++ final_project/index.js
@@ -4,36 +4,6 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
-let users = []
-
-// Check if a user with the given username already exists
-const doesExist = (username) => {
-    // Filter the users array for any user with the same username
-    let userswithsamename = users.filter((user) => {
-        return user.username === username;
-    });
-    // Return true if any user with the same username is found, otherwise false
-    if (userswithsamename.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-// Check if the user with the given username and password exists
-const authenticatedUser = (username, password) => {
-    // Filter the users array for any user with the same username and password
-    let validusers = users.filter((user) => {
-        return (user.username === username && user.password === password);
-    });
-    // Return true if any valid user is found, otherwise false
-    if (validusers.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 const app = express();
 
 app.use(express.json());
@@ -57,26 +27,6 @@ app.use("/customer/auth/*", function auth(req, res, next) {
     } else {
         return res.status(403).json({ message: "User not logged in" });
     }
-});
-
-// Register a new user
-app.post("/register", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // Check if both username and password are provided
-    if (username && password) {
-        // Check if the user does not already exist
-        if (!doesExist(username)) {
-            // Add the new user to the users array
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
-        } else {
-            return res.status(404).json({message: "User already exists!"});
-        }
-    }
-    // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
 });
  
 const PORT =5000;
