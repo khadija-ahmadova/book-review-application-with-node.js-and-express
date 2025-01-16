90752fbc21840420acc225de5fad45adfd99386c
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-01-16 06:38:05 -0500

    implement login

--- final_project/index.js
+++ final_project/index.js
@@ -59,6 +59,33 @@ app.use("/customer/auth/*", function auth(req, res, next) {
     }
 });
 
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+});
+
+
 // Register a new user
 app.post("/register", (req, res) => {
     const username = req.body.username;
