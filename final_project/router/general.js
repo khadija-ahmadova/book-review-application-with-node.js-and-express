58b404657a9240c5518a3ce78c0865972a2645bb
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-01-16 13:55:14 -0500

    implement registration

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,19 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post('/register', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (!isValid(username)) {
+        users.push({"username" : username, "password" : password});
+        return res.status(200).json({message: "User successfully registered. Now you can login"});
+    } else {
+        return res.status(404).json({message: "User already exists"});
+    }
+  }
+
+  return res.status(404). json({message: "Username or password not provided."});
 });
 
 // Get the book list available in the shop
